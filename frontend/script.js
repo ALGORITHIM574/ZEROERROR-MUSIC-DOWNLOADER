a60013e1a0c2637f1fb8da81bea7b8c1192c175b
ALGORITHIM574 <githinjidci0437@gmail.com>
2026-09-15 22:19:55 +0300

    rectified the reload probleam and also solved the merge of video audio error

--- frontend/script.js
+++ frontend/script.js
@@ -5,40 +5,58 @@ const videoBtn = document.getElementById("videoBtn");
 const downloadBtn = document.getElementById("downloadBtn");
 const statusBox = document.getElementById("statusBox");
 let selectedType = null;
-audioBtn.addEventListener("click", () => {
+audioBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  e.stopPropagation();
   console.log("AUDIO BUTTON CLICKED");
   selectedType = "audio";
   audioBtn.classList.add("active");
   videoBtn.classList.remove("active");
 });
-videoBtn.addEventListener("click", () => {
+videoBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  e.stopPropagation();
   console.log("VIDEO CLICKED");
   selectedType = "video";
   videoBtn.classList.add("active");
   audioBtn.classList.remove("active");
 });
-downloadBtn.addEventListener("click", async () => {
+downloadBtn.addEventListener("click", async (e) => {
+  e.preventDefault();
+  e.stopPropagation();
   console.log("DOWNLOAD BUTTON CLICKED");
   const url = videoUrl.value;
   console.log(url);
   const type = selectedType;
   console.log("URL:", url);
+  if (!url) {
+    alert("please enter a youtube url");
+    return;
+  }
+  if (!type) {
+    alert(
+      "please select either Audio or Video before pressing the download button",
+    );
+    return;
+  }
+  console.log("URL:", url);
   console.log("TYPE:", type);
+  try {
+    const response = await fetch("http://localhost:3001/api/download", {
+      method: "POST",
 
-  const response = await fetch("http://localhost:3001/api/download", {
-    method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
 
-    headers: {
-      "Content-Type": "application/json",
-    },
-
-    body: JSON.stringify({
-      url: url,
-      type: type,
-    }),
-  });
-
-  console.log(response);
-  const data = await response.json();
-  console.log(data);
+      body: JSON.stringify({
+        url: url,
+        type: type,
+      }),
+    });
+    const data = await response.json();
+    console.log("server Response", data);
+  } catch (error) {
+    console.error("Fetch failed error layout reset:", error);
+  }
 });
